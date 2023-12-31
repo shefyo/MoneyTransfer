da183e231a99ab9fc9b1516a9d9201f24e14536e
shefyo <110970148+shefyo@users.noreply.github.com>
2023-12-31 21:29:54 +0300

    Обновление работы комиссии

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,9 @@
 fun main() {
-    var amount: Int = 10000
-    var commission: Double = (amount * 0.75) / 100
-    var result: Double = amount + commission
+    var amount: Int = 100
+    var commission: Int = ((amount * 0.75) / 100).toInt()
+    if (commission < 35) {
+        commission = 35
+    }
+    var result: Int = amount + commission
     println("Комиссия равна: " + commission + " рублей." + " Вместе с комиссией вы должны будете заплатить: " + result + " рублей.")
 }
